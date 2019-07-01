54d6fc3d4ecb1f4d3b1f81c0f60dc972944ef0d9
Dominic Petrick <dompetrick@gmail.com>
2019-07-01 16:41:59 +0200

    Bump centos image

--- src/docker.rb
+++ src/docker.rb
@@ -100,7 +100,7 @@ class DockerCommands
     Command.new("docker", "run",
       '-w', '/root/build',
       '-v', "#{context.server_root_path}:/root/build",
-      'prismagraphql/build-image:centos6-0.3',
+      'prismagraphql/build-image:centos6-0.4',
       'cargo', 'build', "--release", "--manifest-path=prisma-rs/Cargo.toml").puts!.run!.raise!
   end
 
