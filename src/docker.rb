9981a1848d0c42c4277fce9b1bb1ce00b1217099
Dominic Petrick <dompetrick@gmail.com>
2019-07-01 13:19:04 +0200

    Bump version of lambda image.

--- src/docker.rb
+++ src/docker.rb
@@ -100,7 +100,7 @@ class DockerCommands
     Command.new("docker", "run",
       '-w', '/root/build',
       '-v', "#{context.server_root_path}:/root/build",
-      'prismagraphql/build-image:lambda',
+      'prismagraphql/build-image:lambda-1.0',
       'cargo', 'build', "--release", "--manifest-path=prisma-rs/Cargo.toml").puts!.run!.raise!
   end
 
